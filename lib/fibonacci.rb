eefd26ba0b914d82e75ea45b727e41d15f3d65a5
Parvathy Iyer <paro_v@yahoo.com>
2015-02-01 08:59:15 -0500

    Update fibonacci.rb

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,7 +1,7 @@
 require 'benchmark'
 
 def fibonacci(n)
-    n <=1 ? n : fibonacci(n-1)+fibonacci(n-2)
+    n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2)
 end
 
 
@@ -11,13 +11,13 @@ end
 
 # faster, iterative method
 def fiboiter(n)
-    temp1=0
-    temp2=1
-    fib=0
+    temp1 = 0
+    temp2 = 1
+    fib = 0
     (2..n).each do
-        fib=temp1+temp2
-        temp1=temp2
-        temp2=fib
+        fib = temp1 + temp2
+        temp1 = temp2
+        temp2 = fib
     end
     fib
 end
